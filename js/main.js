637f23332184da6085f7f007356ed91047b64a2c
Jóhanna Svala Rafnsdóttir <johannasvala@johannas-mbp.lan>
2018-10-23 22:35:49 +0000

    arrays verkefni tilbúið

--- js/main.js
+++ js/main.js
@@ -1,48 +1,21 @@
 console.log("halló")
 var myDiv = document.querySelector(".container")
 
-//Hérna koma input reitirnir -----------------------
+//Hérna koma input reitirnir til að fylla inn í  -----------------------
 var form = document.querySelector(".form")
 form.innerHTML = `
 <div id="form">
     <ul class="list">
-        <label>Name: </label><input id="name" type="text" />
-        <label>Description: </label><input id="description" type="text" />
-        <label>Clicks: </label><input id="clicks" type="text" />
-        <label>Status: </label><input id="status" type="text" />
-        <label>Picture url: </label><input id="pic" type="text" />
-        <button class="button">ADD CARD</button>
+        <label>Nafn: </label><input id="name" type="text" />
+        <label>Starfstitill: </label><input id="description" type="text" />
+        <label>Email: </label><input id="clicks" type="text" />
+        <label>Sími: </label><input id="status" type="text" />
+        <label>Mynda-url: </label><input id="pic" type="text" />
+        <button class="button">Bæta við korti</button>
     </ul>
 </div>
 `
-
-//Hérna fyrir neðan kemur CARD-ið---------------------------------------------
-var names = ["Bjarni", "Jón Oddur", "Guttormur", "Brjánn", "Arrey"];
-var about = [ "Big tempered, tall, friendly", "Sweet, shaved, young", "Shaved, silverfox, giving", "Short, adorable, a loner", "Goal oriented, abitious, optimistic",];
-var photos = [
-    "https://secure.i.telegraph.co.uk/multimedia/archive/03571/potd-squirrel_3571152k.jpg",
-    "http://static.boredpanda.com/blog/wp-content/uploads/2016/03/i-photographed-wild-red-squirrels-celebrating-easter__880.jpg",
-    "https://secure.i.telegraph.co.uk/multimedia/archive/03600/potd-squirrels_3600130k.jpg",
-    "https://us.123rf.com/450wm/geertweggen/geertweggen1706/geertweggen170600392/79866688-close-up-of-red-squirrel-on-a-shopping-cart-with-eggs.jpg?ver=6",
-    "https://c1.staticflickr.com/5/4797/26903324668_484bf3b023_b.jpg"
-];
-var clicks = [45, 60, 3, 356, 1001];
-var available = ["single", "single", "taken", "taken", "single"];
-
-//Hér kemur ARRAY loopan -----------------------------------
-for(var i = 0; i<names.length; i++){ 
-    console.log(i);
-    myDiv.innerHTML += `
-    <div>
-        <h1>${names[i]}</h1>
-        <p class="about">Qualities:  ${about[i]}</p>
-        <p class="clicks">Profile has been clicked: ${clicks[i]} times</p>
-        <p class="avalible">Relationship status: ${available[i]}</p>
-        <img width="650px" height ="350px" src="${photos[i]}">
-    </div>
-    `
-}
-
+//Hérna koma forsendurnar fyrir spjaldið
 var button = document.querySelector(".button");
 var list = document.querySelector(".list");
 var title = document.querySelector("#name");
@@ -52,26 +25,21 @@ var status = document.querySelector("#status");
 var description = document.querySelector("#description");
 
 var addCard = function(e){
-    console.log("halló aftur");
-    
-    myDiv.innerHTML += getSquirrel(title.value, description.value, clicks.value, status.value, picture.value,);
-}
-button.onclick = addCard;
 
-var getSquirrel = function(title, about, clicks, available, photos){
-    var boxEvent = `
+    //Hérna kemur spjaldið sem á að búa til 
+    var getStarfsmann = function(title, about, clicks, available, photos){
+    var starfsmannaCard = `
     <div>
-        <h1>${title}</h1>
-        <p class="about">Qualities: ${about}</p>
-        <p class="clicks">Profile has been clicked: ${clicks} times</p>
-        <p class="avalible">Relationship status: ${available}</p>
+        <h2>${title}</h2>
+        <p class="about">Starfstitill: ${about}</p>
+        <p class="clicks">Email: ${clicks}</p>
+        <p class="avalible">Sími: ${available}</p>
         <img width="650px" height ="350px" src="${photos}">
     </div>
     `
-    return boxEvent;
+    return starfsmannaCard;
+    }
+    myDiv.innerHTML += getStarfsmann(title.value, description.value, clicks.value, status.value, picture.value,);
 }
-console.log("búin að keyra for loop");
 
-
- 
-
+button.onclick = addCard;
